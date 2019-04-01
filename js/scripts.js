1b35eae336db55aff55f91f22e18844f6210e683
Ian Christner and Zema Kebede <zema@>
2019-04-01 14:20:45 -0700

    Append sorted items to ul

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
-  var myString = "";
+  var myRay = [];
   $('#groceryForm').submit(function(event){
     event.preventDefault();
     var input = ["1", "2", "3", "4", "5"];
     console.log(input);
     input.forEach(function(element) {
+      console.log(element);
       var myInput = $('#input' + element).val();
-      myString += myInput.toString()+',';
-  
+      myRay.push(myInput);
+      $("#groceries").append('<li>'+myInput+'</li>');
+      // $("#groceries").append('<li></li>');
+      // $('#groceries').eq(element).text('test');
       });
-alert(myString);
-
     });
   });
+  //myRay.sort();
